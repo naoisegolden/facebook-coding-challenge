98fd2fc5a56cea2c63e54e221a020fae0007d61f
Naoise Golden <naoise.golden@xing.com>
2014-12-29 22:46:08 +0100

    Initial javascript and folder structure

---
+++ assets/javascript/application.js
@@ -0,0 +1,13 @@
+/**
+ * Main code for the application.
+ *
+ * @author Naoise Golden <naoise.golden@gmail.com> (https://github.com/naoisegolden)
+ */
+
+/**
+ * Global function to be called to display events in a calendar
+ *
+ * @param {Array} events A list of objects with a start and end attribute.
+ */
+function layOutDay(events) {
+}
